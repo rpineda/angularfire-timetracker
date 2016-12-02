25ee9cbb5060526a3e1015d34b489367db61612e
Ricardo Pineda <pinedasoft@gmail.com>
2016-12-02 11:46:02 -0600

    Update values.js
    Add Security Bundle Author and url for FIrebase

--- app/services/values.js
+++ app/services/values.js
@@ -1,11 +1,13 @@
 (function () {
 
     angular.module('appTimeTracker').value('appSettings', {
-        applicationName: 'Real-time Time Tracker App',
+        applicationName: 'Code 202 - Time Tracker App',
         applicationUrl: 'http://www.jesperlundnielsen.dk/real-time-app-with-angularjs-firebase/',
         developerName: 'Jesper Lund Nielsen',
         developerUrl: 'http://www.jesperlundnielsen.dk/',
-        firebaseUrl: 'https://timetrackerapp-jln-130315.firebaseio.com'
+        developerSecurityBundle: 'Ricardo Pineda',
+        developerSecurityBundleUrl: 'http://www.code202.com/',
+        firebaseUrl: 'https://code502-4a693.firebaseio.com'
     });
 
 }());
